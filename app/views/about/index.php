bd948c18af0e0060756d1180e6ff54a0e37530e4
robbyobet7 <muhamadrobby24@gmail.com>
2024-10-22 00:33:23 +0700

    Membuat Aplikasi MVC dengan PHP #5 View

---
+++ app/views/about/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+</head>
+<body>
+    <h1>About Me</h1>
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>. </p>
+</body>
+</html>
