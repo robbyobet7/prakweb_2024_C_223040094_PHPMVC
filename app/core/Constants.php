4575de33602f43cc903bf0cf7997089f3004a40c
robbyobet7 <muhamadrobby24@gmail.com>
2024-10-28 19:47:04 +0700

    Vid 6 Assets

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php 
+
+define('BASEURL', 'http://localhost/prakweb_2024_C_223040094_PHPMVC/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman About</title>
-</head>
-<body>
-    <h1>About Me</h1>
+<div class="container">
+
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpg" alt="M. Robby A" width="200" class="rounded-circle shadow">
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>. </p>
-</body>
-</html>
+
+</div>
