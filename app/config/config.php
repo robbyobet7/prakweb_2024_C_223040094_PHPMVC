b83019f0083de8e67e2958510ca9c04d68856062
robbyobet7 <muhamadrobby24@gmail.com>
2024-10-28 20:41:49 +0700

    MVC dengan PHP #8 Database Wrapper

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_2024_C_223040094_PHPMVC/public');
+
+// DATABASE
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
--- app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php 
-
-define('BASEURL', 'http://localhost/prakweb_2024_C_223040094_PHPMVC/public');
